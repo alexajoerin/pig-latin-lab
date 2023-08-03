a50966352c3b1df891f17fa432240f57f14a0951
Alexa Joerin <alexa.joerin@gmail.com>
2023-08-03 09:32:43 -0600

    with extended challenges

--- src/piglatin.js
+++ src/piglatin.js
@@ -40,19 +40,50 @@
 
 // WAY THAT MAKES MOST SENSE TO ME
 
-const translate = (word) => {
-  word = word.toLowerCase();
-  let vowels = ["a", "e", "i", "o", "u"];
-  if (vowels.includes(word[0])) {
-    return `${word}way`;
-  } else {
-    for (let i = 0; i < word.length; i++) {
-      if (vowels.includes(word[i])) {
-        return `${word.slice(i, word.length)}${word.slice(0, i)}ay`;
-      }
-    }
-  }
-};
+// const translate = (word) => {
+//   word = word.toLowerCase();
+//   let vowels = ["a", "e", "i", "o", "u"];
+//   if (vowels.includes(word[0])) {
+//     return `${word}way`;
+//   } else {
+//     for (let i = 0; i < word.length; i++) {
+//       if (vowels.includes(word[i])) {
+//         return `${word.slice(i, word.length)}${word.slice(0, i)}ay`;
+//       }
+//     }
+//   }
+// };
+
+// DON'T WANT TO CONTINUE THIS WAY BECAUSE THERE ARE AN UNDISCLOSED AMOUNT OF CONSONANTS
+
+// const translate = (word) => {
+//   word = word.toLowerCase();
+//   let vowels = ["a", "e", "i", "o", "u"];
+//   if (vowels.includes(word[0])) {
+//     return `${word}way`;
+//   } else if (vowels.includes(word[1])) {
+//     return word.substr(1) + word[0] + "ay";
+//   } else if (vowels.includes(word[2])) {
+//     return word.substr(2) + word.slice(0, 2) + "ay";
+//   }
+// };
+
+// const translate = (word) => {
+//   word = word.toLowerCase();
+//   let vowels = ["a", "e", "i", "o", "u"];
+//   //   we put this condition first because it is the smallest case. We want to do smallest to start and broadest as the else
+//   if (/[0-9@]/.test(word)) {
+//     return word;
+//   } else if (vowels.includes(word[0])) {
+//     return `${word}way`;
+//   } else {
+//     for (let i = 0; i < word.length; i++) {
+//       if (vowels.includes(word[i])) {
+//         return word.slice(i) + word.slice(0, i) + "ay";
+//       }
+//     }
+//   }
+// };
 
 // const translate = (word) => {
 //   for (let i = 0; i < word.length; i++) {
@@ -68,7 +99,7 @@ const translate = (word) => {
 //   }
 // };
 
-// SMALLEST AMOUT OF CODE
+// SMALLEST AMOUT OF CODE - STUDY THIS:
 
 // const translate = (word) => {
 //   const vowels = ["a", "e", "i", "o", "u"];
@@ -80,6 +111,40 @@ const translate = (word) => {
 //     return word.slice(index) + word.slice(0, index) + "ay";
 //   }
 // };
+
+// const translate = (word) => {
+//   word = word.toLowerCase();
+//   if (/[0-9@&$#!]/.test(word)) {
+//     return word;
+//   } else if (/[aeiou]/.test(word[0])) {
+//     return word + "way";
+//   } else {
+//     const wordArr = word.split("");
+//     const index = wordArr.findIndex((char) => /[aeiou]/.test(char));
+//     return word.slice(index) + word.slice(0, index) + "ay";
+//   }
+// };
+
+// HOW TO DO WHAT WE DID ABOVE WITHOUT THE ARRAY
+
+const translate = (dialogue) => {
+  //   dialogue = dialogue.toLowerCase();
+  wordArr = dialogue.split(" ");
+  //   the i added to the regex below makes the test ignore upper or lower case to keep it the same. had to change the tests to keep the  uppercase in them to make it work
+  const vowelsRegex = /[aeiou]/i;
+  const pigArray = [];
+  wordArr.forEach((word) => {
+    if (/[0-9@&$#!]/.test(word)) {
+      pigArray.push(word);
+    } else if (vowelsRegex.test(word[0])) {
+      pigArray.push(word + "way");
+    } else {
+      const index = word.search(vowelsRegex);
+      pigArray.push(word.slice(index) + word.slice(0, index) + "ay");
+    }
+  });
+  return pigArray.join(" ");
+};
 
 module.exports = {
   translate,
--- tests/piglatin.test.js
+++ tests/piglatin.test.js
@@ -14,13 +14,28 @@ describe("translate function", () => {
   test("word with two consonants", () => {
     expect(translate("space")).toBe("acespay");
   });
-  test("word that starts with one consonant", () => {
+  test("word that starts with three consonants", () => {
     expect(translate("strap")).toBe("apstray");
   });
   test("words that start with vowels and have a uppercase letter", () => {
-    expect(translate("Enchant")).toBe("enchantway");
+    expect(translate("Enchant")).toBe("Enchantway");
   });
   test("words that start with consonants and have an uppercase letter", () => {
-    expect(translate("Black")).toBe("ackblay");
+    expect(translate("Black")).toBe("ackBlay");
+  });
+  test("allow punctuation in the input string", () => {
+    expect(translate("can't")).toBe("an'tcay");
+  });
+  test("Don't translate words that have numbers or symbols", () => {
+    expect(translate("189")).toBe("189");
+  });
+  test("Don't translate words that have numbers or symbols", () => {
+    expect(translate("hello@grandcircus.co")).toBe("hello@grandcircus.co");
+  });
+  test("each word is translated in a phrase", () => {
+    expect(translate("a quick brown fox")).toBe("away uickqay ownbray oxfay");
+  });
+  test("each word is translated in a phrase", () => {
+    expect(translate("A Quick Brown Fox")).toBe("Away uickQay ownBray oxFay");
   });
 });
